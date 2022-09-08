f8f2c2eb445127c457addc01452f7eaf36fd60f1
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-08 14:45:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('public'))
+const publicPath = path.join(__dirname, "public");
+
+app.use("/", express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 let listenPort = process.env.PORT || 3000;
 
